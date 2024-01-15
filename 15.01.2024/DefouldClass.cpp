142bf670a8d95b77809114718ccc7905081395f2
Viktoria01281997 <156118373+Viktoria01281997@users.noreply.github.com>
2024-01-15 19:39:36 +0300

    Add files via upload

---
+++ 15.01.2024/DefouldClass.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class Student {
+private:
+	string name;
+	int age;
+public:
+	Student(string name, int age) {
+		this->age = age;
+		this->name = name;
+	}
+	void show() {
+		cout << "name: " << name << " age: " << age << endl;
+	}
+};
+
+int main() {
+
+	Student st1("Pol", 23);
+	st1.show();
+
+	return 0;
+}                                                                                                                                                                                                                          
